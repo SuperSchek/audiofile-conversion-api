2fee1f97a75830826d9d1d0c7c490bbda6a971a6
SuperSchek <sanderschekman@gmail.com>
2018-02-08 16:31:51 +0100

    Implements upload to S3. Wraps up

--- index.js
+++ index.js
@@ -3,18 +3,27 @@ const express = require('express'),
       path = require('path'),
       fileUpload = require('express-fileupload'),
       fs = require('fs'),
+      AWS = require('aws-sdk'),
       app = express(),
       port = process.env.PORT || 3000;
 
+// Load env vars from .env file if not in production.
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
+
+const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
+const AWS_S3_USER_KEY = process.env.AWS_S3_USER_KEY;
+const AWS_S3_USER_SECRET = process.env.AWS_S3_USER_SECRET;
 
 app.use(fileUpload());
 
 app.listen(port, () => {
     console.log('API is running on: ' + port);
 
-    app.post('/upload', (req, res) => {
+    app.post('/convert', (req, res) => {
+        // Download file from post request to server.
         let getFile = function(data) {
-            console.log('getFile');
             return new Promise(function(resolve, reject) {
                 if (!req.files)
                     return res.status(400).send('No files were uploaded.');
@@ -23,7 +32,7 @@ app.listen(port, () => {
 
                 const submitted_file = req.files.audio;
                 podcast.extension = submitted_file.name.split('.').pop();
-                podcast.filename = (req.body.category + '_' + req.body.name).replace(/\s/g, '');
+                podcast.filename = (req.body.category + '_' + req.body.name).replace(/[\s':|,;!&]/g, ''); //Remove any spaces, comma's or colons from filename.
 
                 let moveFile = function(obj) {
                     return new Promise(function(resolve, reject) {
@@ -31,103 +40,91 @@ app.listen(port, () => {
                             if (err)
                                 return res.status(500).send(err);
 
-                            // res.send('File uploaded');
                             resolve(podcast);
                         });
                     });
                 };
             
-                moveFile().then(function(obj) {                  
+                moveFile().then(function(obj) {
+                    console.log('File received.');
                     return resolve(obj);
                 })
 
             });
         };
 
-        let secondProm = function(obj) {
+        // Convert audiofile to a 128kbps .mp3 file.
+        let convertFile = function(obj) {
             return new Promise(function(resolve, reject) {
+                console.log('Converting ' + obj.filename + '_original.' + obj.extension + ' to 128kbps mp3 using ffmpeg!')
                 ffmpeg(obj.filename + '_original.' + obj.extension)
                 .audioBitrate('128')
                 .toFormat('mp3')
                 .on('error', function (err) {
                     return res.status(500).send(err);
-                    console.log('An error occurred: ' + err.message);
+                    console.error('An error occurred: ' + err.message);
                 })
                 .on('progress', function (progress) {
                     // console.log(JSON.stringify(progress));
                     console.log('Processing: ' + progress.targetSize + ' KB converted');
                 })
                 .on('end', function () {
-                    console.log('Processing finished !');
+                    console.log('Processing finished.');
                     resolve(obj);                    
                 })
-                .save('./hello.mp3')//path where you want to save your file
-
-                // res.sendFile(path.join(__dirname, '/', obj.filename + '_original.' + obj.extension), function() {
-                //     fs.unlinkSync(obj.filename + '_original.' + obj.extension);
-                //     resolve(obj);
-                // });
-                
+                .save(obj.filename + '.mp3')//path where you want to save your file
             });
         };
 
-        let thirdProm = function(obj) {
+        // Delete original file.
+        let deleteOriginal = function(obj) {
             return new Promise(function(resolve, reject) {
-                res.sendFile(path.join(__dirname, '/',  'hello.mp3'), function() {
-                    fs.unlinkSync(obj.filename + '_original.' + obj.extension);
-                    fs.unlinkSync('hello.mp3');                    
-                    resolve(obj);
-                });
+                fs.unlinkSync(obj.filename + '_original.' + obj.extension);
+                resolve(obj);               
             });
         };
 
         getFile().then(function(result) {
-            return secondProm(result);
+            return convertFile(result);
         }).then(function(result) {
-            return thirdProm(result);
+            return deleteOriginal(result);
         }).then(function(obj) {
-            console.log("Done: Sending file now!");
-            console.log(obj);
-            console.log('sent');
+            uploadToS3(obj);
         });
+
+        function uploadToS3(obj) {
+            console.log("Uploading " + obj.filename + ".mp3 to AWS S3!");            
+            let s3bucket = new AWS.S3({
+                accessKeyId: AWS_S3_USER_KEY,
+                secretAccessKey: AWS_S3_USER_SECRET,
+                Bucket: AWS_S3_BUCKET_NAME,
+            });
+            fs.readFile(obj.filename + '.mp3', function(err, data) {
+                if (err)
+                    console.error(err);
+
+                s3bucket.createBucket(function() {
+                    const params = {
+                        Bucket: AWS_S3_BUCKET_NAME,
+                        Key: 'wp-content/' + (new Date()).getFullYear() + '/' + obj.filename + '.mp3',
+                        Body: data,
+                        ACL: "public-read",
+                    };
+                    s3bucket.upload(params, function(err, data) {
+                        if (err)
+                            console.error(err);
+                        
+                        obj.s3_url = data.Location;
+                        obj.message = 'Succesfully converted and uploaded ' + obj.filename + '.mp3 to AWS S3.'
+                        console.log('Succesfully uploaded ' + obj.filename + '.mp3 to S3!');
+            
+                        res.status(200).send(obj);
+                        fs.unlinkSync(obj.filename + '.mp3');
+                    });
+                });
+            });
+        }
+        
     });
 
-    // app.post('/upload', (req, res) => {
-    //     // Check for files
-    //     if (!req.files)
-    //         return res.status(400).send('No files were uploaded.');
-
-    //     const submitted_file = req.files.audio;
-    //     const extension = submitted_file.name.split('.').pop();
-    //     const filename = (req.body.category + '_' + req.body.name).replace(/\s/g, '');
-
-    //     function upload() {
-    //         return new Promise(function(resolve, reject) {
-    //             submitted_file.mv(filename + '.' + extension, function(err) {
-    //                 if (err)
-    //                     return res.status(500).send(err);
-    //                 // res.send('File uploaded');
-    //                 console.log('uploaded');
-    //                 resolve();
-    //             });
-    //         });
-    //     }
-
-    //     function postFile() {
-    //         return new Promise(function(resolve, reject) {
-    //             res.sendFile(path.join(__dirname, '/', filename + '.' + extension));
-    //             console.log('sent');
-    //             resolve();
-    //         });
-    //     };
-        
-    //     upload().then(function() {
-    //         // res.sendFile("./Review_Molly'sGame.mp3");
-    //         postFile().then(function() {
-    //             console.log('deleting');
-    //             fs.unlinkSync(filename + '.' + extension);
-    //         });
-    //     });
-
-    // });
 });
---
+++ readme.md
@@ -0,0 +1,14 @@
+This is a small REST API built using nodejs and express. It takes a post request with an audiofile which will then convert the received file to 128kbps and upload it to AWS S3. After this it will return an object containing the URL to the converted file on S3. Meant for deployment to Heroku.
+
+The following enviroment variables should be set:
+NODE_ENV=  #Should be set to production on production, can be left empty otherwise
+AWS_S3_BUCKET_NAME=  #Name of S3 bucket
+AWS_S3_USER_KEY=  #AWS User key with the proper permissions
+AWS_S3_USER_SECRET=  #AWS User secret with the proper permissions
+
+Request should be posted to /convert. It needs the following properties in the body:
+{
+    audio: [AUDIO FILE]
+    category: [PODCAST CATEGORY]
+    name: [NAME OF EPISODE]
+}
